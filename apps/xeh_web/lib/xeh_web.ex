40787182d31350cd9017bd24ac745230ed61ceaa
Wojtek Mach <wojtek@wojtekmach.pl>
2016-02-11 03:04:28 +0100

    xeh_web: Store packages in Registry

--- apps/xeh_web/lib/xeh_web.ex
+++ apps/xeh_web/lib/xeh_web.ex
@@ -9,7 +9,9 @@ defmodule XehWeb do
     Logger.info("Starting cowboy on port #{port}")
     Plug.Adapters.Cowboy.http(XehWeb.App, [], port: port)
 
-    children = []
+    children = [
+      worker(XehWeb.Registry, [])
+    ]
 
     opts = [strategy: :one_for_one, name: XehWeb.Supervisor]
     Supervisor.start_link(children, opts)
@@ -18,14 +20,33 @@ end
 
 defmodule XehWeb.App do
   import Plug.Conn
+  alias XehWeb.Registry
 
   def init(options), do: options
 
   def call(conn, _opts) do
-    packages = []
+    packages = Registry.list
 
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Poison.encode!(packages))
+  end
+end
+
+defmodule XehWeb.Registry do
+  def start_link do
+    Agent.start_link(fn -> MapSet.new end, name: __MODULE__)
+  end
+
+  def clear do
+    Agent.update(__MODULE__, fn _ -> MapSet.new end)
+  end
+
+  def list do
+    Agent.get(__MODULE__, fn packages -> packages end) |> Enum.sort
+  end
+
+  def put(package) do
+    Agent.update(__MODULE__, fn packages -> MapSet.put(packages, package) end)
   end
 end
--- apps/xeh_web/test/xeh_web_test.exs
+++ apps/xeh_web/test/xeh_web_test.exs
@@ -1,8 +1,20 @@
 defmodule XehWebTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   use Plug.Test
+
+  setup do
+    XehWeb.Registry.clear
+    :ok
+  end
 
   test "list packages: none" do
     assert XehClient.list == []
+  end
+
+  test "list packages: all" do
+    XehWeb.Registry.put("package 1")
+    XehWeb.Registry.put("package 2")
+
+    assert XehClient.list == ["package 1", "package 2"]
   end
 end
